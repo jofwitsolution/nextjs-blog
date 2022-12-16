2e0c48069a50ae3349185657787ffa95f797ad96
Faleye Oluwafemi joseph <faleyeoluwafemi1@gmail.com>
2022-12-16 15:56:58 +0100

    all done

---
+++ pages/examples/example.js
@@ -0,0 +1,9 @@
+import Alert from '../../components/alert';
+
+export default function Example() {
+  return (
+    <Alert type={'error'}>
+      <p>This is the main paragraph</p>
+    </Alert>
+  );
+}
